4d83401e50ec313eb2d38fe1d3dcb59827b576e3
aida bising <aidaashumie@gmail.com>
2020-08-03 22:06:41 +0800

    login logout bihhh

---
+++ admin/paper-dashboard-master/examples/mainMenu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<title> Staff Training System </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+	<center> <h2> WHO ARE YOU? </h2> <center>
+	<div class="w3-cell-row">
+	<div class="w3-container w3-cell" style = "center">
+	<a class = 'w3-btn w3-brown' href = "login.php" > ADMIN </a> </p>
+	</div>
+	
+	<center> <div class="w3-container w3-cell">
+	<a class = 'w3-btn w3-brown' href = "login.php" > STAFF </a> </p>
+	</div>
+	</div>
+	</center>
+	
+</body>
+</html>
