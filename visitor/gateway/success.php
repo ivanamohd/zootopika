f4fae3a881ad02288f988a9290842299123145a7
ivana <ivana.mohd@gmail.com>
2020-10-04 18:45:41 +0800

    buang reference kat success

--- visitor/gateway/success.php
+++ visitor/gateway/success.php
@@ -22,7 +22,6 @@
   <div class="container mt-4">
     <h2>Thank you for purchasing your tickets at Zootopika</h2>
     <hr>
-    <p>Your transaction ID is <?php echo $tid; ?></p>
     <p>Check your email for more info</p>
     <p><a href="../index.html" class="btn btn-light mt-2">Go Back</a></p>
   </div>
